248a0c18331c8c4a253dc79b0123f6312aa6f99d
Coderast <Coderast@example.com>
2018-07-23 15:42:41 +0300

    Taylor 15/16 degree sin/cos complited

--- Piligrim/src/math/optimal/fastmath.h
+++ Piligrim/src/math/optimal/fastmath.h
@@ -5,6 +5,7 @@ namespace piligrim {
 		class FastMath
 		{
 		public:
+			// sin and cos make by Taylor approximation | sin - 15 degree; cos - 16 degree
 			static double sin(double alpha);
 			static float sinf(float alpha);
 
