525eeb8272676265f38e81d42b41a0dce60ca688
Coderast <Coderast@example.com>
2018-07-23 14:45:31 +0300

    Init optimal math functions

---
+++ Piligrim/src/math/optimal/fastmath.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace piligrim {
+	namespace math {
+		class FastMath
+		{
+		public:
+			static double sin(double alpha);
+			static float sinf(float alpha);
+
+			static double cos(double alpha);
+			static float cosf(float alpha);
+
+
+		};
+	}
+}
