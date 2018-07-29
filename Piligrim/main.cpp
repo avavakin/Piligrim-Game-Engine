417ce7d6c4cb80c3b99d374251d08e251b43018b
Coderast <Coderast@example.com>
2018-07-29 19:51:39 +0300

    Added mini-cube light source and changed mesh from square to cube

--- Piligrim/main.cpp
+++ Piligrim/main.cpp
@@ -9,9 +9,13 @@
 
 #include "src\graphics\buffers\buffers.h"
 #include "src\graphics\renderer\camera\camera.h"
+//
+#include <ctime>
 
 #define WINDOW_WIDTH 1000
 #define WINDOW_HIGHT (WINDOW_WIDTH * 9 / 16)
+
+#define SIZE__ 1048576 * 100
 
 int main()
 {
@@ -22,59 +26,83 @@ int main()
 
 	Window window("Scott Piligrim vs. World!", WINDOW_WIDTH, WINDOW_HIGHT);
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	float xMarg = 0, yMarg = 0;
-
+	float cubeEdge = 10;
+	cubeEdge /= 2;
 	float pointsSource[] =
 	{
-		-.5 + xMarg, -.5 + yMarg, 0,
-		0.5 + xMarg, -.5 + yMarg, 0,
-		-.5 + xMarg, 0.5 + yMarg, 0,
-		0.5 + xMarg, 0.5 + yMarg, 0
+		cubeEdge, cubeEdge, cubeEdge,
+		-cubeEdge, cubeEdge, cubeEdge,
+		-cubeEdge, -cubeEdge, cubeEdge,
+		cubeEdge, -cubeEdge, cubeEdge,
+		cubeEdge, -cubeEdge, -cubeEdge,
+		cubeEdge, cubeEdge, -cubeEdge,
+		-cubeEdge, cubeEdge, -cubeEdge,
+		-cubeEdge, -cubeEdge, -cubeEdge
 	};
 
 	unsigned int indeciesSource[] =
 	{
 		0, 1, 2,
-		2, 3, 1
+		2, 3, 0,
+		3, 5, 0,
+		3, 4, 5,
+		4, 7, 5,
+		7, 5, 6,
+		2, 1, 6,
+		7, 2, 6,
+		1, 5, 6,
+		1, 0, 5,
+		2, 4, 7,
+		2, 3, 4
 	};
 		
-	Mesh figureOne(pointsSource, sizeof(pointsSource) / sizeof(float) / 3, indeciesSource, sizeof(indeciesSource) / sizeof(unsigned int), { -.5f, 0.f, 0.0f });
-	Mesh figureTwo(pointsSource, sizeof(pointsSource) / sizeof(float) / 3, indeciesSource, sizeof(indeciesSource) / sizeof(unsigned int), { 3.75f, 3.f, 1.f});
+	Mesh figureOne(pointsSource, sizeof(pointsSource) / sizeof(float) / 3, indeciesSource, sizeof(indeciesSource) / sizeof(unsigned int), { 0.f, 0.f, 0.0f });
+
+	Mesh lightSource(pointsSource, sizeof(pointsSource) / sizeof(float) / 3, indeciesSource, sizeof(indeciesSource) / sizeof(unsigned int), {cubeEdge*4, cubeEdge*4, cubeEdge*4});
+
+	VertexArray spriteFigure, spriteLightSource;
+	IndexBuffer ibo(figureOne.getIndecies(), figureOne.getCountIndecies());
+
+	spriteFigure.addBuffer(new Buffer(figureOne.getLocalPoints(), figureOne.getCountPoints() * 3, 3), 0);
+
+	spriteLightSource.addBuffer(new Buffer(lightSource.getLocalPoints(), lightSource.getCountPoints() * 3, 3), 0);
 
 
-	float * const coords = figureOne.getLocalPoints();
-	unsigned int* indecies = figureOne.getIndecies();
-
-	VertexArray spriteOne, spriteTwo;
-	IndexBuffer ibo(&indecies[0], figureOne.getCountIndecies());
-
-	spriteOne.addBuffer(new Buffer(coords, figureOne.getCountPoints() * 3, 3), 0);
-
-	spriteTwo.addBuffer(new Buffer(coords, figureTwo.getCountPoints() * 3, 3), 0);
-
-
-	// Shader BEGIN
+	// Shaders BEGIN
 	std::string shaderErrorMessage;
-	Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag", shaderErrorMessage);
-	if (!shader.isOk())
+	Shader shaderMesh("src/shaders/basic.vert", "src/shaders/basic.frag", shaderErrorMessage);
+	if (!shaderMesh.isOk())
 	{
 		LOG_ER(shaderErrorMessage);
 		system("pause");
 		return 0;
 	}
 	LOG(shaderErrorMessage);
-	shader.enable();
+	shaderMesh.enable();
+
 
 	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 	mat4 persp = mat4::perspective(45.0f, static_cast<float>(WINDOW_WIDTH)/WINDOW_HIGHT, 0.1f, 100.0f);
 
-	shader.setUniform("u_pr_matrix", persp);
+	shaderMesh.setUniform("u_pr_matrix", persp);
 
-	shader.setUniform("u_color", vec4(0.2, 0.8, 0.2, 1));
+	shaderMesh.setUniform("u_color", vec4(0.2, 0.8, 0.2, 1));
+
+
+	Shader shaderLight("src/shaders/basic.vert", "src/shaders/light.frag", shaderErrorMessage);
+	if (!shaderLight.isOk())
+	{
+		LOG_ER(shaderErrorMessage);
+		system("pause");
+		return 0;
+	}
+	LOG(shaderErrorMessage);
+	shaderLight.enable();
+	shaderLight.setUniform("u_pr_matrix", persp);
 	// Shader END
-	Camera cam;
+	Camera cam(lightSource.getPosition(), vec3(0,0,0));
 
-	float camSpeed = 5;
+	float camSpeed = 50;
 
 	float pitch = 0;
 	float yaw = -90;
@@ -154,17 +182,30 @@ int main()
 			cam.setPosition(cam.getPosition() + vec3(0.f, 1.f, 0.f) * camSpeed * deltaTime);
 		}
 
-		shader.enable();
-		const vec3& meshCenter = figureOne.getPosition();
+		shaderMesh.enable();
+		shaderMesh.setUniform("u_vw_matrix", cam.getMatrix());
+		spriteFigure.bind();
 
-		shader.setUniform("u_vw_matrix", cam.getMatrix());
-
-		spriteOne.bind();
 		ibo.bind();
-		shader.setUniform("u_ml_matrix", mat4::translation(meshCenter));
+		shaderMesh.setUniform("u_ml_matrix", mat4::translation(figureOne.getPosition()));
 		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_INT, 0);
+		ibo.unbind();
+
+		spriteFigure.unbind();
+		shaderMesh.disable();
+
+		shaderLight.enable();
+		shaderLight.setUniform("u_vw_matrix", cam.getMatrix());
+		spriteLightSource.bind();
+
 		ibo.bind();
-		spriteOne.unbind();
+		shaderLight.setUniform("u_ml_matrix", mat4::translation(lightSource.getPosition()) * mat4::scale(vec3(0.2f, 0.2f, 0.2f)));
+		shaderLight.setUniform("light_color", vec4(1.0,1.0,1.0,1.0));
+		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_INT, 0);
+		ibo.unbind();
+
+		spriteLightSource.unbind();
+		shaderLight.disable();
 
 		window.update();
 	}
