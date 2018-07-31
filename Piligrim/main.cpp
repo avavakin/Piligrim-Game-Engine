289fd269d49b469b03d73c259a729d098afc6f90
Coderast <Coderast@example.com>
2018-07-31 16:32:37 +0300

    Added demo of lightning

--- Piligrim/main.cpp
+++ Piligrim/main.cpp
@@ -9,13 +9,9 @@
 
 #include "src\graphics\buffers\buffers.h"
 #include "src\graphics\renderer\camera\camera.h"
-//
-#include <ctime>
 
 #define WINDOW_WIDTH 1000
 #define WINDOW_HIGHT (WINDOW_WIDTH * 9 / 16)
-
-#define SIZE__ 1048576 * 100
 
 int main()
 {
@@ -26,46 +22,104 @@ int main()
 
 	Window window("Scott Piligrim vs. World!", WINDOW_WIDTH, WINDOW_HIGHT);
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	glEnable(GL_DEPTH_TEST);
 	float cubeEdge = 10;
 	cubeEdge /= 2;
-	float pointsSource[] =
+	float lightPointsSource[] =
 	{
 		cubeEdge, cubeEdge, cubeEdge,
 		-cubeEdge, cubeEdge, cubeEdge,
+
 		-cubeEdge, -cubeEdge, cubeEdge,
 		cubeEdge, -cubeEdge, cubeEdge,
+
 		cubeEdge, -cubeEdge, -cubeEdge,
 		cubeEdge, cubeEdge, -cubeEdge,
+
 		-cubeEdge, cubeEdge, -cubeEdge,
 		-cubeEdge, -cubeEdge, -cubeEdge
 	};
 
-	unsigned int indeciesSource[] =
+	float meshPointsSource[] =
 	{
+		-cubeEdge, -cubeEdge, -cubeEdge,  0.0f,  0.0f, -1.0f,
+		cubeEdge, -cubeEdge, -cubeEdge,  0.0f,  0.0f, -1.0f,
+		cubeEdge,  cubeEdge, -cubeEdge,  0.0f,  0.0f, -1.0f,
+		cubeEdge,  cubeEdge, -cubeEdge,  0.0f,  0.0f, -1.0f,
+		-cubeEdge,  cubeEdge, -cubeEdge,  0.0f,  0.0f, -1.0f,
+		-cubeEdge, -cubeEdge, -cubeEdge,  0.0f,  0.0f, -1.0f,
+
+		-cubeEdge, -cubeEdge,  cubeEdge,  0.0f,  0.0f,  1.0f,
+		cubeEdge, -cubeEdge,  cubeEdge,  0.0f,  0.0f,  1.0f,
+		cubeEdge,  cubeEdge,  cubeEdge,  0.0f,  0.0f,  1.0f,
+		cubeEdge,  cubeEdge,  cubeEdge,  0.0f,  0.0f,  1.0f,
+		-cubeEdge,  cubeEdge,  cubeEdge,  0.0f,  0.0f,  1.0f,
+		-cubeEdge, -cubeEdge,  cubeEdge,  0.0f,  0.0f,  1.0f,
+
+		-cubeEdge,  cubeEdge,  cubeEdge, -1.0f,  0.0f,  0.0f,
+		-cubeEdge,  cubeEdge, -cubeEdge, -1.0f,  0.0f,  0.0f,
+		-cubeEdge, -cubeEdge, -cubeEdge, -1.0f,  0.0f,  0.0f,
+		-cubeEdge, -cubeEdge, -cubeEdge, -1.0f,  0.0f,  0.0f,
+		-cubeEdge, -cubeEdge,  cubeEdge, -1.0f,  0.0f,  0.0f,
+		-cubeEdge,  cubeEdge,  cubeEdge, -1.0f,  0.0f,  0.0f,
+
+		cubeEdge,  cubeEdge,  cubeEdge,  1.0f,  0.0f,  0.0f,
+		cubeEdge,  cubeEdge, -cubeEdge,  1.0f,  0.0f,  0.0f,
+		cubeEdge, -cubeEdge, -cubeEdge,  1.0f,  0.0f,  0.0f,
+		cubeEdge, -cubeEdge, -cubeEdge,  1.0f,  0.0f,  0.0f,
+		cubeEdge, -cubeEdge,  cubeEdge,  1.0f,  0.0f,  0.0f,
+		cubeEdge,  cubeEdge,  cubeEdge,  1.0f,  0.0f,  0.0f,
+
+		-cubeEdge, -cubeEdge, -cubeEdge,  0.0f, -1.0f,  0.0f,
+		cubeEdge, -cubeEdge, -cubeEdge,  0.0f, -1.0f,  0.0f,
+		cubeEdge, -cubeEdge,  cubeEdge,  0.0f, -1.0f,  0.0f,
+		cubeEdge, -cubeEdge,  cubeEdge,  0.0f, -1.0f,  0.0f,
+		-cubeEdge, -cubeEdge,  cubeEdge,  0.0f, -1.0f,  0.0f,
+		-cubeEdge, -cubeEdge, -cubeEdge,  0.0f, -1.0f,  0.0f,
+
+		-cubeEdge,  cubeEdge, -cubeEdge,  0.0f,  1.0f,  0.0f,
+		cubeEdge,  cubeEdge, -cubeEdge,  0.0f,  1.0f,  0.0f,
+		cubeEdge,  cubeEdge,  cubeEdge,  0.0f,  1.0f,  0.0f,
+		cubeEdge,  cubeEdge,  cubeEdge,  0.0f,  1.0f,  0.0f,
+		-cubeEdge,  cubeEdge,  cubeEdge,  0.0f,  1.0f,  0.0f,
+		-cubeEdge,  cubeEdge, -cubeEdge,  0.0f,  1.0f,  0.0f
+	};
+
+	unsigned int lightIndeciesSource[] =
+	{
+		// FRONT
 		0, 1, 2,
-		2, 3, 0,
+		2, 0, 3,
+		// RIGHT
 		3, 5, 0,
 		3, 4, 5,
+		// BACK
 		4, 7, 5,
 		7, 5, 6,
+		// LEFT
 		2, 1, 6,
 		7, 2, 6,
+		// TOP
 		1, 5, 6,
-		1, 0, 5,
+		1, 5, 0,
+		// BOTTOM
 		2, 4, 7,
 		2, 3, 4
 	};
-		
-	Mesh figureOne(pointsSource, sizeof(pointsSource) / sizeof(float) / 3, indeciesSource, sizeof(indeciesSource) / sizeof(unsigned int), { 0.f, 0.f, 0.0f });
 
-	Mesh lightSource(pointsSource, sizeof(pointsSource) / sizeof(float) / 3, indeciesSource, sizeof(indeciesSource) / sizeof(unsigned int), {cubeEdge*4, cubeEdge*4, cubeEdge*4});
+	vec3 figureCenter(0,0,0);
+	vec3 lightCenter;
 
 	VertexArray spriteFigure, spriteLightSource;
-	IndexBuffer ibo(figureOne.getIndecies(), figureOne.getCountIndecies());
+	IndexBuffer lightIbo(lightIndeciesSource, sizeof(lightIndeciesSource) / sizeof(unsigned int));
 
-	spriteFigure.addBuffer(new Buffer(figureOne.getLocalPoints(), figureOne.getCountPoints() * 3, 3), 0);
+	spriteFigure.setBuffer(new Buffer(meshPointsSource, sizeof(meshPointsSource)/sizeof(float)));
+	spriteFigure.addBufferAttributes(0, 3, 6 * sizeof(float), 0);
+	spriteFigure.addBufferAttributes(1, 3, 6 * sizeof(float), 3 * sizeof(float));
 
-	spriteLightSource.addBuffer(new Buffer(lightSource.getLocalPoints(), lightSource.getCountPoints() * 3, 3), 0);
+	spriteLightSource.setBuffer(new Buffer(lightPointsSource, sizeof(lightPointsSource)/sizeof(float)));
+	spriteLightSource.addBufferAttributes(0, 3, 0, 0);
 
 
 	// Shaders BEGIN
@@ -80,16 +134,15 @@ int main()
 	LOG(shaderErrorMessage);
 	shaderMesh.enable();
 
-
-	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 	mat4 persp = mat4::perspective(45.0f, static_cast<float>(WINDOW_WIDTH)/WINDOW_HIGHT, 0.1f, 100.0f);
 
 	shaderMesh.setUniform("u_pr_matrix", persp);
 
-	shaderMesh.setUniform("u_color", vec4(0.2, 0.8, 0.2, 1));
+	shaderMesh.setUniform("u_object_color", vec3(0.2, 0.8, 0.2));
+	shaderMesh.setUniform("u_light_color", vec3(1, 1, 1));
 
 
-	Shader shaderLight("src/shaders/basic.vert", "src/shaders/light.frag", shaderErrorMessage);
+	Shader shaderLight("src/shaders/light.vert", "src/shaders/light.frag", shaderErrorMessage);
 	if (!shaderLight.isOk())
 	{
 		LOG_ER(shaderErrorMessage);
@@ -100,7 +153,7 @@ int main()
 	shaderLight.enable();
 	shaderLight.setUniform("u_pr_matrix", persp);
 	// Shader END
-	Camera cam(lightSource.getPosition(), vec3(0,0,0));
+	Camera cam(vec3(0,0,cubeEdge*2), vec3(0,0,0));
 
 	float camSpeed = 50;
 
@@ -115,6 +168,7 @@ int main()
 	double x, y;
 	double offsetX, offsetY;
 	double sensitivity = 0.9f;
+	float lightRad = cubeEdge * 4;
 	while (!window.closed())
 	{
 		currentTime = glfwGetTime();
@@ -122,9 +176,9 @@ int main()
 		lastTime = currentTime;
 
 		window.clear();
-
+		/*
 		window.getMousePosition(x, y);
-
+		
 		offsetX = x - lastX;
 		offsetY = lastY - y;
 
@@ -157,11 +211,11 @@ int main()
 		}
 
 		cam.setLookDir(camDir);
-
+		*/
 		if (window.isKeyPressed(GLFW_KEY_ESCAPE)) {
 			break;
 		}
-
+		/*
 		if (window.isKeyPressed(GLFW_KEY_W)) {
 			cam.setPosition(cam.getPosition() + cam.getLookDir() * camSpeed * deltaTime);
 		}
@@ -181,30 +235,50 @@ int main()
 		if (window.isKeyPressed(GLFW_KEY_SPACE)) {
 			cam.setPosition(cam.getPosition() + vec3(0.f, 1.f, 0.f) * camSpeed * deltaTime);
 		}
+		*/
+
 
 		shaderMesh.enable();
 		shaderMesh.setUniform("u_vw_matrix", cam.getMatrix());
+		lightCenter = vec3(
+			cos(glfwGetTime())*lightRad, 
+			cos(glfwGetTime())*lightRad, 
+			sin(glfwGetTime())*lightRad
+		);
+		cam.set(
+			vec3(
+				cos(glfwGetTime())*lightRad*2,
+				sin(glfwGetTime())*lightRad*2,
+				sin(glfwGetTime())*lightRad*2
+			),
+			figureCenter
+		);
+
+		shaderMesh.setUniform("u_light_pos", lightCenter);
+		shaderMesh.setUniform("u_cam_pos", cam.getPosition());
+		shaderMesh.setUniform("u_ml_matrix", mat4::translation(figureCenter));
+
 		spriteFigure.bind();
 
-		ibo.bind();
-		shaderMesh.setUniform("u_ml_matrix", mat4::translation(figureOne.getPosition()));
-		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_INT, 0);
-		ibo.unbind();
+		glDrawArrays(GL_TRIANGLES, 0, sizeof(meshPointsSource) / sizeof(float) / 2 / 3);
 
 		spriteFigure.unbind();
+
 		shaderMesh.disable();
 
 		shaderLight.enable();
 		shaderLight.setUniform("u_vw_matrix", cam.getMatrix());
+		shaderLight.setUniform("u_ml_matrix", mat4::translation(lightCenter) * mat4::scale(vec3(0.2f, 0.2f, 0.2f)));
+		shaderLight.setUniform("light_color", vec4(1.0, 1.0, 1.0, 1.0));
+
 		spriteLightSource.bind();
 
-		ibo.bind();
-		shaderLight.setUniform("u_ml_matrix", mat4::translation(lightSource.getPosition()) * mat4::scale(vec3(0.2f, 0.2f, 0.2f)));
-		shaderLight.setUniform("light_color", vec4(1.0,1.0,1.0,1.0));
-		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_INT, 0);
-		ibo.unbind();
+		lightIbo.bind();
+		glDrawElements(GL_TRIANGLES, lightIbo.getCount(), GL_UNSIGNED_INT, 0);
+		lightIbo.unbind();
 
 		spriteLightSource.unbind();
+
 		shaderLight.disable();
 
 		window.update();
