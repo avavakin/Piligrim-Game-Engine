76a6bed0719767b4572758705271c49a1f4236e7
Coderast <Coderast@example.com>
2018-08-13 01:19:04 +0300

    Added Time Check macros

--- Piligrim/src/utils/logs.h
+++ Piligrim/src/utils/logs.h
@@ -1,5 +1,14 @@
 #pragma once
+#include <ctime>
 
-#define LOG(x) std::cout << x << std::endl;
+#define LOG(x) std::cout << (x) << std::endl;
 
-#define LOG_ER(x) std::cerr << "[ERROR] " << x << std::endl;
+#define LOG_ER(x) std::cerr << "[ERROR] " << (x) << std::endl;
+
+#define LOG_TIME_CHECK_S(x) unsigned int start = clock(); \
+x; \
+std::cout << "[TIME CHECK] " << #x << " :: " << (clock() - start) / 1000.0f << " s" << std::endl
+
+#define LOG_TIME_CHECK_MS(x)  unsigned int start = clock(); \
+x; \
+std::cout << "[TIME CHECK] "  << #x << " :: " << (clock() - start) << " ms" << std::endl
